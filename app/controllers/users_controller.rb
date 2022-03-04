db7a914cc76ebab21fb4731084149009fe3b9219
ford burgess <fburg6824@gmail.com>
2022-03-04 01:30:26 +0000

    profile edit exists but does not work

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -17,6 +17,17 @@ class UsersController < ApplicationController
     @chatroom = Chatroom.find_by(creator: current_user, invited: @user) || Chatroom.find_by(creator: @user, invited: current_user)
   end
 
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    @user.update(user_params)
+
+    redirect_to users_path
+  end
+
   private
 
   def user_params
