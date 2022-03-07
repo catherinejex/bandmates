72dc1bff7bfd65431fae5824f7988255da7ebe0e
Minja Slavkovic <minja.slavkovic@gmail.com>
2022-03-07 21:47:29 +0000

    fixed messages and new post page

---
+++ app/javascript/favourite.js
@@ -0,0 +1,7 @@
+// const musicNote = document.querySelector('.music-note');
+
+// musicNote.addEventListener('click', (event) => {
+//   musicNote.style.color = 'rgb(195, 6, 195)'
+// })
+
+// export {favourite};
